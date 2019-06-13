3417dd8f8e31bed2751b8cee71b7d44ae02a1134
Akash Ravi <akashkravi@gmail.com>
2019-06-13 17:51:28 +0530

    Removed frontend modules for isolated testing
    Signed-off-by: Akash Ravi <akashkravi@gmail.com>

--- extension/packagemanagerextension/__init__.py
+++ extension/packagemanagerextension/__init__.py
@@ -1,24 +1,3 @@
-def _jupyter_nbextension_paths():
-    """Used by 'jupyter nbextension' command to install frontend extension"""
-    return [dict(
-        section="notebook",
-        # the path is relative to the `extension` directory
-        src="nbextension",
-        # directory in the `nbextension/` namespace
-        dest="packagemanagerextension",
-        # _also_ in the `nbextension/` namespace
-        require="packagemanagerextension/extension"),
-
-        dict(
-        section="tree",
-        # the path is relative to the `extension` directory
-        src="nbextension",
-        # directory in the `nbextension/` namespace
-        dest="packagemanagerextension",
-        # _also_ in the `nbextension/` namespace
-        require="packagemanagerextension/extension")]
-
-
 def _jupyter_server_extension_paths():
     """Used by "jupyter serverextension" command to install web server extension'"""
     return [{
