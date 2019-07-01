ca76d4c39edeee25b338a98ed6a356e6a0cd0ffc
Akash Ravi <akashkravi@gmail.com>
2019-07-02 01:04:21 +0530

    [UI] added sidebar frontend
    Signed-off-by: Akash Ravi <akashkravi@gmail.com>

--- extension/packagemanagerextension/nbextension/tree.js
+++ extension/packagemanagerextension/nbextension/tree.js
@@ -13,10 +13,10 @@ define(function (require) {
             $('<link>')
                 .attr('rel', 'stylesheet')
                 .attr('type', 'text/css')
-                .attr('href', urls.static_url + 'conda.css')
+                .attr('href', urls.static_url + 'sidebar.css')
         );
 
-        utils.ajax(urls.static_url + 'tab.html', {
+        utils.ajax(urls.static_url + 'sidebar.html', {
             dataType: 'html',
             success: function (env_html, status, xhr) {
                 // Configure Conda tab
@@ -26,28 +26,12 @@ define(function (require) {
                         .append(
                             $('<a>')
                                 .attr('id', 'package_manager_tab')
-                                .attr('href', '#package_manager')
-                                .attr('data-toggle', 'tab')
                                 .text('Package Manager')
                                 .click(function (e) {
-                                    window.history.pushState(null, null, '#package_manager');
-                                    models.environments.load();
-                                    models.available.load();
+                                    document.getElementById("mySidenav").style.width = "400px";
                                 })
                         )
                 );
-
-                views.EnvView.init();
-                views.AvailView.init();
-                views.InstalledView.init();
-
-                models.environments.view = views.EnvView;
-                models.available.view = views.AvailView;
-                models.installed.view = views.InstalledView;
-
-                if (window.location.hash === '#package_manager') {
-                    $('#package_manager_tab').click();
-                }
             }
         });
     }
