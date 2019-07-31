a8877aacd9de0dfb47f151c9f10d56eb1951e1ea
Akash Ravi <akashkravi@gmail.com>
2019-07-31 23:51:40 +0530

    [JQUERY] Replace hardcoded HTML
    Signed-off-by: Akash Ravi <akashkravi@gmail.com>

--- extension/js/methods.js
+++ extension/js/methods.js
@@ -24,7 +24,7 @@ function update_packages(project) {
     $('#updatebtn').unbind();
     $('#updatebtn').click(function () {
         var selectedPackages = common.get_selected_packages();
-        var html = "<p> The following packages are about to be updated: </p> </br>";
+        var html = $('<p> The following packages are about to be updated: </p> </br>');
         $('#updatebtn').toggleClass('fa-wrench fa-spinner').addClass('fa-spin');
         api.check_update(project, function (resp) {
             $('#updatebtn').toggleClass('fa-wrench fa-spinner').removeClass('fa-spin');
@@ -33,7 +33,8 @@ function update_packages(project) {
             var ct = 0;
 
             if (selectedPackages.length > 0) {
-                html += "<ul>";
+                html.append($('<ul/>'));
+
                 for (var i = 0; i < selectedPackages.length; i++) {
                     var element = selectedPackages[i];
                     element = element.split('=');
@@ -42,17 +43,14 @@ function update_packages(project) {
                     var nver = check_new_version(updates, pkg, ver);
                     if (nver != ver) {
                         packages.push(pkg);
-                        html += "<li>";
-                        html += pkg + "    " + ver + " -> " + nver;
-                        html += "</li>";
+                        html.append("<li>" + pkg + "    " + ver + " -> " + nver + "</li>");
                         ct += 1;
                     }
                 }
-                html += "</ul>";
             }
             else {
                 var list = $('.installed-values');
-                html += "<ul>";
+                html.append($('<ul/>'));
                 for (var i = 0; i < list.length; i++) {
                     var element = list[i];
                     var pkg = $(element).find('.value-name')[0].innerText;
@@ -60,20 +58,17 @@ function update_packages(project) {
                     var nver = check_new_version(updates, pkg, ver);
                     if (nver != ver) {
                         packages.push(pkg);
-                        html += "<li>";
-                        html += pkg + "    " + ver + " -> " + nver;
-                        html += "</li>";
+                        html.append("<li>" + pkg + "    " + ver + " -> " + nver + "</li>");
                         ct += 1;
                     }
                 }
-                html += "</ul>";
             }
             if (ct === 0) {
-                html = "<p> There are no updates available </p>"
-                common.dispay_msg("Install Packages", $.parseHTML(html));
+                html = $("<p> There are no updates available </p>");
+                common.dispay_msg("Install Packages", html);
             }
             else
-                common.confirm("Update Packages", $.parseHTML(html), "Confirm", function () {
+                common.confirm("Update Packages", html, "Confirm", function () {
                     api.update_packages(packages, project);
                 });
         });
@@ -90,24 +85,21 @@ function delete_packages(project) {
     $('#deletebtn').unbind();
     $('#deletebtn').click(function () {
         var selectedPackages = common.get_selected_packages();
-        var html = "<p> The following packages are about to be deleted: </p> </br>";
-        html += "<ul>";
+        var html = $("<p> The following packages are about to be deleted: </p> </br>");
+        html.append($('<ul/>'));
         var packages = [];
         for (var i = 0; i < selectedPackages.length; i++) {
             var element = selectedPackages[i];
             element = element.split('=')[0];
             packages.push(element);
-            html += "<li>";
-            html += element;
-            html += "</li>";
+            html.append("<li>" + element + "</li>");
         }
-        html += "</ul>";
         if (packages.length === 0) {
-            html = "<p> No packages selected </p>";
-            common.dispay_msg("Install Packages", $.parseHTML(html));
+            html = $("<p> No packages selected </p>");
+            common.dispay_msg("Install Packages", html);
         }
         else
-            common.confirm("Delete Packages", $.parseHTML(html), "Confirm", function () {
+            common.confirm("Delete Packages", html, "Confirm", function () {
                 api.delete_packages(packages, project);
             });
     });
@@ -122,22 +114,19 @@ function install_packages(project) {
     $('#installbtn').unbind();
     $('#installbtn').click(function () {
         var toInstall = common.get_to_install();
-        var html = "<p> The following packages are about to be added: </p> </br>";
-        html += "<ul>";
+        var html = $("<p> The following packages are about to be added: </p> </br>");
+        html.append($('<ul/>'));
         for (var i = 0; i < toInstall.length; i++) {
             var element = toInstall[i];
             element = element.split('=')[0];
-            html += "<li>";
-            html += element;
-            html += "</li>";
+            html.append("<li>" + element + "</li>");
         }
-        html += "</ul>";
         if (toInstall.length === 0) {
-            html = "<p> No packages selected </p>";
-            common.dispay_msg("Install Packages", $.parseHTML(html));
+            html = $("<p> No packages selected </p>");
+            common.dispay_msg("Install Packages", html);
         }
         else
-            common.confirm("Install Packages", $.parseHTML(html), "Confirm", function () {
+            common.confirm("Install Packages", html, "Confirm", function () {
                 api.install_packages(toInstall, project);
             });
     });
--- extension/js/scripts.js
+++ extension/js/scripts.js
@@ -39,7 +39,7 @@ function search_view() {
     $('#package-name').unbind();
 
     var typingTimer;                //timer identifier
-    var doneTypingInterval = 1000;  //time in ms, 5 second for example
+    var doneTypingInterval = 1500;  //time in ms, 1.5 seconds for example
 
     $('#package-name').on('keyup', function () {
         clearTimeout(typingTimer);
@@ -49,33 +49,20 @@ function search_view() {
                 // Do not query if the string size is too small. This will save a lot of time.
                 return;
             }
+            $('#searchlist').empty();
             $('#searchicon').toggleClass('fa-search fa-dot-circle-o').addClass('Blink');
             api.search(query, function (res) {
                 var pks = res.packages;
-                var html = "";
-                $('#searchlist').html(html);
                 for (var i = 0; i < pks.length; i++) {
                     var element = pks[i];
                     var name = element.name;
                     var version = element.version;
                     var entry = name + " - " + version;
-                    html += "<option value='" + entry + "'>";
-                    html += entry;
-                    html += "</option>";
+                    var opt = $("<option>").val(entry).text(entry);
+                    $('#searchlist').append(opt);
                 }
                 $('#searchicon').toggleClass('fa-search fa-dot-circle-o').removeClass('Blink');
-                $('#searchlist').html(html);
 
-                $('input[list="searchlist"]').each(function () {
-                    var elem = $(this),
-                        list = $('#searchlist');
-                    elem[0].value = elem[0].value.split('=')[0];
-                    elem.autocomplete({
-                        source: list.children().map(function () {
-                            return $(this).text();
-                        }).get()
-                    });
-                });
             })
         }, doneTypingInterval);
     });
